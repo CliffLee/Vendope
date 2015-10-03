e2f167e7927bd21b6c94db9ddd7a1a6d91a69f63
CliffLee <cliffordlee1997@gmail.com>
2015-10-03 11:46:21 -0700

    Initial Commit

---
+++ daf/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  daf
+//
+//  Created by Clifford Lee on 10/3/15.
+//  Copyright © 2015 Clifford Lee. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
