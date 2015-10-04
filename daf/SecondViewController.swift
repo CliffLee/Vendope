d8a112d6deda7df9ad77f63bce951a4a4b4ee612
anupkrish24 <anupkrish24@gmail.com>
2015-10-03 18:13:04 -0700

    implemented temp list

--- daf/SecondViewController.swift
+++ daf/SecondViewController.swift
@@ -8,13 +8,28 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchResultsUpdating, UISearchBarDelegate {
+    
+    @IBOutlet weak var tblSearchResults: UITableView!
+    
+    var dataArray = [String] ()
+    var filteredArray = [String]()
+    var shouldShowSearchResults = false
+    var searchController: UISearchController!
+    
     var appDelegate:AppDelegate?
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         appDelegate = UIApplication.sharedApplication().delegate as? AppDelegate
         appDelegate?.setRootViewController()
+        
+        loadListOfServices()
+        
+        tblSearchResults.delegate = self
+        tblSearchResults.dataSource = self
+        
+        configureSearchController()
     }
 
     override func didReceiveMemoryWarning() {
@@ -27,6 +42,106 @@ class SecondViewController: UIViewController {
         
         appDelegate.centerContainer!.toggleDrawerSide(MMDrawerSide.Left, animated: true, completion: nil)
     }
+    
+    func configureSearchController() {
+        searchController = UISearchController(searchResultsController: nil)
+        searchController.searchResultsUpdater = self
+        searchController.dimsBackgroundDuringPresentation = true
+        searchController.searchBar.placeholder = "Search here..."
+        searchController.searchBar.delegate = self
+        searchController.searchBar.sizeToFit()
+        tblSearchResults.tableHeaderView = searchController.searchBar
+        
+        
+    }
+    
+    func searchBarTextDidBeginEditing(searchBar: UISearchBar) {
+        shouldShowSearchResults = true
+        tblSearchResults.reloadData()
+    }
+    
+    
+    
+    func searchBarCancelButtonClicked(searchBar: UISearchBar) {
+        shouldShowSearchResults = false
+        tblSearchResults.reloadData()
+    }
+    
+    func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+        if !shouldShowSearchResults {
+            shouldShowSearchResults = true
+            tblSearchResults.reloadData()
+        }
+        
+        searchController.searchBar.resignFirstResponder()
+    }
+    
+    func updateSearchResultsForSearchController(searchController: UISearchController) {
+        let searchString = searchController.searchBar.text
+        
+        // Filter the data array and get only those countries that match the search text.
+        filteredArray = dataArray.filter({ (country) -> Bool in
+            let countryText: NSString = country
+            
+            return (countryText.rangeOfString(searchString!, options: NSStringCompareOptions.CaseInsensitiveSearch).location) != NSNotFound
+        })
+        // Reload the tableview.
+        tblSearchResults.reloadData()
+    }
+    
+    func loadListOfServices() {
+        // Specify the path to the countries list file.
+        let pathToFile = NSBundle.mainBundle().pathForResource("services", ofType: "txt")
+        
+        if let path = pathToFile {
+            // Load the file contents as a string.
+            let countriesString = try! String(contentsOfFile: path, encoding: NSUTF8StringEncoding)
+            
+            // Append the countries from the string to the dataArray array by breaking them using the line change character.
+            dataArray = countriesString.componentsSeparatedByString("\n")
+            
+            // Reload the tableview.
+            tblSearchResults.reloadData()
+        }
+    }
+    
+    
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if shouldShowSearchResults {
+            return filteredArray.count
+        }
+        else {
+            return dataArray.count
+        }
+    }
+    
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("idCell", forIndexPath: indexPath)
+        
+        if shouldShowSearchResults {
+            cell.textLabel?.text = filteredArray[indexPath.row]
+        }
+        else {
+            cell.textLabel?.text = dataArray[indexPath.row]
+        }
+        
+        return cell
+    }
+    
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 60.0
+    }
+
+    
+    
+        
 
 }
-
